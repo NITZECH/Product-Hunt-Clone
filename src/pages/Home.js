30df2aa64e9b9f6a80ea7e7e5578ace4c26d2f05
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2020-10-16 10:23:59 +0545

    routing and tabs

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {IonPage,
+    IonHeader,
+    IonToolbar,IonTitle} from '@ionic/react'
+
+const Home= () => {
+    return (
+       <IonPage>
+           <IonHeader>
+               <IonToolbar>
+                   <IonTitle>Home</IonTitle>
+               </IonToolbar>
+           </IonHeader>
+       </IonPage>
+    )
+}
+
+export default Home
